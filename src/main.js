cae6a7fc07602395ab57dca8e0f1ae454a377736
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-01-27 16:37:05 +0200

    basic js

---
+++ src/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+//basic variables
+const addBtn = document.querySelector('#add-button');
+const textInput = document.querySelector('#text-input');
+const prioritySelector = document.querySelector('#priority-selector');
+const ulList = document.querySelector("ul");
