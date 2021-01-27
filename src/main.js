fc13a817ca096a8973a281330fc0dce202dbee51
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-01-27 17:50:30 +0200

    add function to add todo tasks plus a bit styling, complete first 2 tests

--- src/main.js
+++ src/main.js
@@ -5,3 +5,31 @@ const addBtn = document.querySelector('#add-button');
 const textInput = document.querySelector('#text-input');
 const prioritySelector = document.querySelector('#priority-selector');
 const ulList = document.querySelector("ul");
+
+//event listeners
+addBtn.addEventListener('click', addTodo);
+
+function addTodo (e) {
+    let inputValue = textInput.value;
+    if (inputValue != "") {
+        textInput.value = '';  //reset the input line
+        const todoContainer = document.createElement("div");  //todo task container in div
+        const todoPriority = document.createElement("div");  //todo priority in div
+        const todoCreatedAt = document.createElement("div");  //time to do created in div
+        const todoText = document.createElement("div");  //todo task text in div
+
+        todoContainer.className = 'todo-container';
+        todoText.className = 'todo-text';
+        todoCreatedAt.className = 'todo-created-at';
+        todoPriority.className = 'todo-priority';
+        
+        let date = new Date();
+        todoCreatedAt.innerHTML = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
+        todoText.innerHTML = inputValue;
+        todoPriority.innerHTML = prioritySelector.value;
+
+        todoContainer.append(todoPriority, todoText, todoCreatedAt);
+        ulList.append(todoContainer);
+    }
+}
