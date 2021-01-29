65d3ef3cfc1d59969c5af22198e0ae8fcd634900
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-01-29 15:58:37 +0200

    switch from local storage to JSONBIN

--- src/main.js
+++ src/main.js
@@ -9,9 +9,10 @@ const sortBtn = document.querySelector('#sort-button');
 let counterTitle = document.querySelector('#counter');
 counterTitle.innerHTML = 'No Tasks To Do';
 let counter = 0;
+let arrOfObjTasks = [];
 
 //event listeners
-document.addEventListener('DOMContentLoaded', getDataFromLocalStorage);
+document.addEventListener('DOMContentLoaded', getTasksFromJSONBin);
 addBtn.addEventListener('click', addTodo);
 sortBtn.addEventListener('click', sortTodoList);
 
@@ -24,7 +25,7 @@ function addTodo (e) {
         const todoPriority = document.createElement('div');  //todo priority in div
         const todoCreatedAt = document.createElement('div');  //time to do created in div
         const todoText = document.createElement('div');  //todo task text in div
-        const deleteBtn = document.createElement('button');
+        const deleteBtn = document.createElement('button');  //delete button 
 
         todoContainer.className = 'todo-container';
         todoText.className = 'todo-text';
@@ -33,25 +34,51 @@ function addTodo (e) {
         deleteBtn.className = 'delete-button';
 
         let date = new Date();
-        todoCreatedAt.innerText = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+        let dateSQLFormat = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+        todoCreatedAt.innerText = dateSQLFormat;
 
         todoText.innerText = inputValue;
         todoPriority.innerText = prioritySelector.value;
         deleteBtn.innerText = 'Delete';
 
+        const taskObj = {
+            text: inputValue,
+            priority: prioritySelector.value,
+            taskCreatedAt: dateSQLFormat
+        }
+        arrOfObjTasks.push(taskObj);
+
         todoContainer.append(todoPriority, todoText, todoCreatedAt, deleteBtn);
         ulList.append(todoContainer);
-        saveLocalStorage(ulList);
+        counter++;
+
+        deleteBtn.addEventListener('click', deleteTodo);
+
         prioritySelector.value = 1;
     } else {
         alert('WRITE A TASK!');
     }
     textInput.focus();
-    counter++;
     counterTasks(counter);
+    updateTaskToJSONBin();
 }
 
-//sort the todo list by priority
+//function to delete tasks
+function deleteTodo (e) {
+    let item = e.target.parentElement;
+    const deletedObj = item.querySelector('.todo-created-at').innerText;
+    arrOfObjTasks = arrOfObjTasks.filter(taskObj => taskObj.taskCreatedAt != deletedObj);
+    item.remove();
+
+    counter--;
+    counterTasks(counter);
+    textInput.focus();
+
+    updateTaskToJSONBin();
+}
+
+
+//function to sort the todo list by priority
 function sortTodoList (e) {
     let arrOfPriority = document.querySelectorAll('.todo-priority');
     let sortArr = [];
@@ -68,59 +95,67 @@ function sortTodoList (e) {
     }
 }
 
-
-//save to local storage
-function saveLocalStorage () {
-    let todos = ulList.querySelectorAll('.todo-container');
-    let arrOfAllTasks = [];
-    for (let i = 0; i < todos.length; i++) {
-        let arrTask = [];
-        arrTask[0] = todos[i].getElementsByClassName('todo-priority')[0].innerText;
-        arrTask[1] = todos[i].getElementsByClassName('todo-text')[0].innerText;
-        arrTask[2] = todos[i].getElementsByClassName('todo-created-at')[0].innerText;
-        arrOfAllTasks.push(arrTask);
-    }
-    let JSONReadyArr = JSON.stringify(arrOfAllTasks);
-    localStorage.setItem("tasks", JSONReadyArr);
-}
-
-// get data from local storage
-function getDataFromLocalStorage () {
-    if (localStorage.tasks) {
-        let arrOfAllTasksBack = JSON.parse(localStorage.getItem("tasks"));
-        for (let i = 0; i < arrOfAllTasksBack.length; i++) {
-            const todoContainer = document.createElement('div');  //todo task container in div
-            const todoPriority = document.createElement('div');  //todo priority in div
-            const todoCreatedAt = document.createElement('div');  //time to do created in div
-            const todoText = document.createElement('div');  //todo task text in div
-            const deleteBtn = document.createElement('button');
-
-            todoContainer.className = 'todo-container';
-            todoText.className = 'todo-text';
-            todoCreatedAt.className = 'todo-created-at';
-            todoPriority.className = 'todo-priority';
-            deleteBtn.className = 'delete-button';
-
-            todoPriority.innerText = arrOfAllTasksBack[i][0];
-            todoText.innerText = arrOfAllTasksBack[i][1];
-            todoCreatedAt.innerText = arrOfAllTasksBack[i][2];
-            deleteBtn.textContent = 'Delete';
-            
-            todoContainer.append(todoPriority, todoText, todoCreatedAt, deleteBtn);
-            ulList.appendChild(todoContainer);
-        }
-            localStorage.setItem("tasks", JSON.stringify(arrOfAllTasksBack));
-            counterTasks(arrOfAllTasksBack.length);
-            counter = arrOfAllTasksBack.length;
-    } else {
-        localStorage.setItem("tasks", []);
-    }
-}
-
+//function to count how much tasks you have 
 function counterTasks (count) {
     if (count === 0) {
         counterTitle.innerHTML = 'No Tasks To Do';
     } else {
         counterTitle.innerHTML = `${count}`;
     }
+}
+
+//function to update tasks in JSONBIN
+async function updateTaskToJSONBin () {
+    fetch('https://api.jsonbin.io/v3/b/6012f2cc050e9474fe36b1e8', {
+    method: 'PUT',
+    headers: {
+    'Content-Type': 'application/json',
+    'X-Master-Key': '$2b$10$3c8HlT7Mkm6Fmhp4/y0UveKGq8qFaFdTiTNKewqhEuXpQ9l7Itxdm',
+  },
+    body: JSON.stringify({"my-todo": arrOfObjTasks}) 
+})
+}
+
+//function to get tasks from JSONBIN   
+async function getTasksFromJSONBin() {
+    let response = await fetch("https://api.jsonbin.io/v3/b/6012f2cc050e9474fe36b1e8", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Master-Key": "$2b$10$3c8HlT7Mkm6Fmhp4/y0UveKGq8qFaFdTiTNKewqhEuXpQ9l7Itxdm",
+      },
+    });
+    let data = await response.json();
+    arrOfObjTasks = data.record["my-todo"];
+    insertTasksToHtml();
+}
+
+//function to insert data from JSONBIN to HTML
+function insertTasksToHtml () {
+    for (let i = 0; i < arrOfObjTasks.length; i++) {
+        const todoContainer = document.createElement('div');  //todo task container in div
+        const todoPriority = document.createElement('div');  //todo priority in div
+        const todoCreatedAt = document.createElement('div');  //time to do created in div
+        const todoText = document.createElement('div');  //todo task text in div
+        const deleteBtn = document.createElement('button');
+
+        todoContainer.className = 'todo-container';
+        todoText.className = 'todo-text';
+        todoCreatedAt.className = 'todo-created-at';
+        todoPriority.className = 'todo-priority';
+        deleteBtn.className = 'delete-button';
+
+        todoPriority.innerText = arrOfObjTasks[i].priority;
+        todoText.innerText = arrOfObjTasks[i].text;
+        todoCreatedAt.innerText = arrOfObjTasks[i].taskCreatedAt;
+        deleteBtn.textContent = 'Delete';
+        
+        todoContainer.append(todoPriority, todoText, todoCreatedAt, deleteBtn);
+        ulList.appendChild(todoContainer);
+        deleteBtn.addEventListener ('click', deleteTodo);
+
+        counterTasks(arrOfObjTasks.length);
+        counter = arrOfObjTasks.length;
+    }
+    textInput.focus();
 }
