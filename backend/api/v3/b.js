bef8b29a317156582a87b5b8cfc15f92400413f8
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-02-17 21:09:54 +0200

    added get method by ID, add another router

---
+++ backend/api/v3/b.js
@@ -0,0 +1,56 @@
+const { json } = require("express");
+const express = require("express");
+const fs = require("fs");
+const router = express.Router();
+
+router.use(express.json());
+
+router.get("/", (request, response) => {
+  let allUsers = fs.readdirSync('./backend/bins');
+  let allBins = [];
+
+  for (let i = 0; i < allUsers.length; i++) {
+    let body = fs.readFileSync(`./backend/bins/${allUsers[i]}`, {encoding:'utf8', flag:'r'});
+    body = JSON.parse(body);
+    allBins.push(body);
+  }
+  response.json(allBins);
+});
+
+router.get("/:id", (request, response) => {
+  const { id } = request.params;
+  let body = fs.readFileSync(`./backend/bins/${id}.json`, {encoding:'utf8', flag:'r'});
+  body = JSON.parse(body);
+  response.json(body);
+});
+
+// router.post("/", (request, response) => {
+//   const { body } = request;
+//   console.log(body);
+//   const id = Date.now();
+//   try {
+//     fs.writeFileSync(
+//       `./backend/bins/${id}.json`,
+//       JSON.stringify(body, null, 4)
+//       );
+//       response.status(201).send(`task added, name:${id}`);
+//   } catch (e) {
+//     response.status(500).json({ message: "Error!", error: e });
+//   }
+// });
+
+// router.put("/:created", (request, response) => {
+//   const { created } = request.params;
+//   const { body } = request;
+//   try {
+//     fs.writeFileSync(
+//       `./greets/greet-${created}.json`,
+//       JSON.stringify(body, null, 4)
+//     );
+//     response.json(body);
+//   } catch (e) {
+//     response.status(500).json({ message: "Error!", error: e });
+//   }
+// });
+
+module.exports = router;
--- backend/api/v3/index.js
+++ backend/api/v3/index.js
@@ -1,54 +1,11 @@
-const { json } = require("express");
-const express = require("express");
-const fs = require("fs");
-const router = express.Router();
+const { Router } = require("express");
+const b = require("./b");
 
+const v3 = Router();
 
-
-router.use(express.json());
-
-router.get("/", (request, response) => {
-  let allUsers = fs.readdirSync('./backend/bins');
-  let allBins = [];
-  
-  for (let i = 0; i < allUsers.length; i++) {
-    let body = fs.readFileSync(`./backend/bins/${allUsers[i]}`, {encoding:'utf8', flag:'r'});
-    body = JSON.parse(body);
-    allBins.push(body);
-  }
-  response.json(allBins);
+v3.use("/b", b);
+v3.use("*", (req, res) => {
+  res.send("Not found! :(");
 });
 
-// router.get("/:name", (request, response) => {
-//   const { name } = request.params;
-//   response.send(`Hello ${name}!`);
-// });
-
-// router.post("/", (request, response) => {
-//   const { body } = request;
-//   try {
-//     fs.writeFileSync(
-//       `./greets/greet-${Date.now()}.json`,
-//       JSON.stringify(body, null, 4)
-//     );
-//     response.status(201).send("greet added");
-//   } catch (e) {
-//     response.status(500).json({ message: "Error!", error: e });
-//   }
-// });
-
-// router.put("/:created", (request, response) => {
-//   const { created } = request.params;
-//   const { body } = request;
-//   try {
-//     fs.writeFileSync(
-//       `./greets/greet-${created}.json`,
-//       JSON.stringify(body, null, 4)
-//     );
-//     response.json(body);
-//   } catch (e) {
-//     response.status(500).json({ message: "Error!", error: e });
-//   }
-// });
-
-module.exports = router;
+module.exports = v3;
